9a770824bd46eaa4a68183b8878f16fc75a30fb8
Tony <ak28402840@gmail.com>
2017-06-06 22:42:24 +0800

    add ignore file, add Constant file

---
+++ Plurtaku/Plurtaku/Constant.swift
@@ -0,0 +1,18 @@
+//
+//  Constant.swift
+//  Plurtaku
+//
+//  Created by 梁政倫 on 2017/5/21.
+//  Copyright © 2017年 ak2840. All rights reserved.
+//
+
+import UIKit
+
+class Constant: NSObject
+{
+    static let APP_KEY = "oHat5lVqqhU0"
+    static let APP_SECRET = "LreVpWI1I6owTEXDrSjgHoZESWFCN87S"
+    static let OAUTH_SIGNATURE_METHOD = "HMAC-SHA1"
+    static let OAUTH_NONCE = "12345"
+    static let OAUTH_VERSION = "1.0"
+}
